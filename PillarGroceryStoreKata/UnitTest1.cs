e5f2ee813f8b42f08743feb62ab4749e28ae8f41
Lynkfox <lynkfox@gmail.com>
2020-02-18 20:38:19 -0500

    Initial Commit

---
+++ PillarGroceryStoreKata/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PillarGroceryStoreKata
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
