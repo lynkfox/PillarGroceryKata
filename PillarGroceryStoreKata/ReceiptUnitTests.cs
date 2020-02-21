90aa4ee824187984edf4cf1f81811bd380fc8028
Lynkfox <lynkfox@gmail.com>
2020-02-21 10:39:57 -0500

    BuyWithMultipleItemsOfDifferentPriceGivesProperTotalAndItemCount Green Already.

--- PillarGroceryStoreKata/ReceiptUnitTests.cs
+++ PillarGroceryStoreKata/ReceiptUnitTests.cs
@@ -36,5 +36,24 @@ namespace PillarGroceryStoreKata
 
             Assert.AreEqual(expectedTotal, testReceipt.Total);
         }
+
+        [TestMethod]
+        public void BuyWithMultipleItemsOfDifferentPriceGivesProperTotalAndItemCount()
+        {
+            var itemRepository = new ItemRepository();
+            itemRepository.Add("Milk", 2.49);
+            itemRepository.Add("Rotini", 5.49);
+            var testReceipt = new Receipt(itemRepository);
+            
+            double expectedTotal = 7.98;
+            int expectedItemCount = 2;
+
+            testReceipt.Buy("Milk");
+            testReceipt.Buy("Rotini");
+
+            Assert.AreEqual(expectedTotal, testReceipt.Total);
+            Assert.AreEqual(expectedItemCount, testReceipt.NumberOfItems);
+
+        }
     }
 }
