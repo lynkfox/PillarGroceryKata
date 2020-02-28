19921d1084e9cea129e8119184b19f11d7883267
Lynkfox <lynkfox@gmail.com>
2020-02-28 11:59:40 -0500

    oops. Bad compile. Fixed!

--- PillarGroceryStoreKata/IntegrationTests.cs
+++ PillarGroceryStoreKata/IntegrationTests.cs
@@ -22,7 +22,7 @@ namespace PillarGroceryStoreKata
 
             testItemRepository.Markdown("Milk", 1);
             testItemRepository.Markdown("Salmon", .49);
-            testItemRepository.BuyNumberGetNumberAtDiscountPercentLimitNumber("TomatoSoup", 2, 1, 1,6);
+            testItemRepository.BuyNumberGetNumberAtDiscountPercentLimitNumber("TomatoSoup", 2, 1, 1, 6);
             testItemRepository.BuyNumberGetNumberAtDiscountPercentLimitNumber("RotiniPasta", 1, 1, .25, 2);
             testItemRepository.BuyNumberGetDiscountPercentOnEqualOrLesser("Salmon", 4, 1, .5);
             testItemRepository.BuyGroupAtReducedPrice("PeanutButter", 3, 5m);
@@ -32,14 +32,14 @@ namespace PillarGroceryStoreKata
             receiptBeingTested.Buy("Onions"); //+1.39 -- Use Case 1
             receiptBeingTested.Buy("GroundBeef", 1, 3.5); //+6.96  -- Use Case 2 
             receiptBeingTested.Buy("Milk"); // +1.49 -- Use Case 3a
-            receiptBeingTested.Buy("Salmon", 1, 3.2); //+25.6 -- Use Case 3b
+            receiptBeingTested.Buy("Salmon", 1, 4); //+37.6 -- Use Case 3b
             receiptBeingTested.Buy("TomatoSoup", 3); //+.98 -- Use Case 4a
             receiptBeingTested.Buy("RotiniPasta", 2); //+10.41 -- Use Case 4b
             receiptBeingTested.Buy("PeanutButter", 3); //+5 -- Use Case 5
             receiptBeingTested.Buy("RotiniPasta"); //+5.95 -- Already Hit Limit, Use Case 6
             receiptBeingTested.Void("Onions"); // -1.39 -- Use Case 7a
             receiptBeingTested.Void("RotiniPasta", 2);  //-5.95 - 4.46 -- Use Case 7b 
-            receiptBeingTested.Buy("Salmon", 1, 1.8); //+7.59 for remaining to 4lbs, +4.75 for remaining lbs -- use case 8
+            receiptBeingTested.Buy("Salmon", 1, 1); //+4.75 -- Use Case 8
 
 
 
@@ -51,7 +51,7 @@ namespace PillarGroceryStoreKata
         {
             var receipt = Setup();
 
-            decimal expectedTotal = 58.32m;
+            decimal expectedTotal = 62.73m;
             int expectedNumberOfItems = 11; //Ground Beef and Salmon only count as 1 each
             string expectedLastItem = "Salmon";
             
